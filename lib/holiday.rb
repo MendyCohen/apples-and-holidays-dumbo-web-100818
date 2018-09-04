777074df202798f9b3c4cfe0204b8a59bb6a1bf1
Mendy <mendybeltcohen@gmail.com>
2018-09-04 19:20:36 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -70,7 +70,7 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
    holiday_hash.collect do |key,value|
     if holiday_hash[key].keys.any?{|key| key == :fourth_of_july || key == :memorial_day}
-     holiday_hash[key].keys
+     holiday_hash[key].flatten.keys
     end
   end
 end
